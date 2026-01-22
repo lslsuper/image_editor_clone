d00b1dabe1b6df7fda9dbf407ffd42e643af41ea
lslsuper <lsl810712914@qq.com>
2026-01-22 23:39:58 +0800

    refactor: update supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -5,6 +5,11 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY! || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      auth: {
+        // Prevent the client from trying to exchange OAuth codes in the browser
+        // because we complete the exchange on the server callback.
+        detectSessionInUrl: false,
+      },
       cookieOptions: {
         sameSite: 'lax',
         secure: process.env.NODE_ENV === 'production',
